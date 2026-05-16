c4ef9a5a7c999ef6f8148cace799a78b7dcca314
shivani-kulandaivel <4shivanikulandaivel@gmail.com>
2026-05-16 13:18:52 -0700

    marked things we'll have to fix later

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const GOOGLE_CLIENT_ID = "818886256150-8dapdbmbhiq57taek1b62mr8veikhafr.apps.googleusercontent.com";
-const GOOGLE_API_KEY = "AIzaSyAg5zU-Tq3ct8eZb8Mo127rS7INYrth1es";
+const GOOGLE_CLIENT_ID = "818886256150-8dapdbmbhiq57taek1b62mr8veikhafr.apps.googleusercontent.com"; // SENSITIVE
+const GOOGLE_API_KEY = "AIzaSyAg5zU-Tq3ct8eZb8Mo127rS7INYrth1es"; // SENSITIVE
 const App = (() => {
   function init() {
     renderToday();
